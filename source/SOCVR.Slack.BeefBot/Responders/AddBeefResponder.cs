86fe704828e204f0efba6a3bdfff61280859cd0a
gunr2171 <kevin@kevin-hunter.net>
2016-01-12 20:08:59 -0500

    adding regex and CanRespond

--- source/SOCVR.Slack.BeefBot/Responders/AddBeefResponder.cs
+++ source/SOCVR.Slack.BeefBot/Responders/AddBeefResponder.cs
@@ -5,14 +5,20 @@ using System.Text;
 using System.Threading.Tasks;
 using MargieBot.Models;
 using MargieBot.Responders;
+using System.Text.RegularExpressions;
 
 namespace SOCVR.Slack.BeefBot.Responders
 {
     class AddBeefResponder : IResponder
     {
+        Regex commandPattern = new Regex(@"(?i)^beef (low|medium|high) (\d+) (.+)?$", RegexOptions.Compiled | RegexOptions.CultureInvariant);
+
         public bool CanRespond(ResponseContext context)
         {
-            throw new NotImplementedException();
+            return
+                commandPattern.IsMatch(context.Message.Text) && //  Must match command regex.
+                !context.Message.User.IsSlackbot && // Message must be said by a non-bot.
+                context.Message.MentionsBot; // Message must mention the bot.
         }
 
         public BotMessage GetResponse(ResponseContext context)
