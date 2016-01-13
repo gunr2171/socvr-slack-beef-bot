611830854ce5cd5540b6fa862a1d6b8a65fe0096
gunr2171 <kevin@kevin-hunter.net>
2016-01-12 20:21:30 -0500

    starting to add the EF model

---
+++ source/SOCVR.Slack.BeefBot/Database/BeefEntry.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOCVR.Slack.BeefBot.Database
+{
+    public class BeefEntry
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public BeefSeverity Severity { get; set; }
+
+        [Required]
+        public string ReporterUserId { get; set; }
+
+        public int OffendingChatUserId { get; set; }
+
+        public string Explination { get; set; }
+
+        public DateTimeOffset ReportedOn { get; set; }
+        public DateTimeOffset ExpiresOn { get; set; }
+    }
+
+    public enum BeefSeverity
+    {
+        Low = 1,
+        Medium = 2,
+        High = 3
+    }
+}
---
+++ source/SOCVR.Slack.BeefBot/Database/DatabaseContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOCVR.Slack.BeefBot.Database
+{
+    public class DatabaseContext : DbContext
+    {
+        private string connectionString;
+
+        public DatabaseContext(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public DbSet<BeefEntry> BeefEntries { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(connectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
